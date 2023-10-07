590074475c0478e613703d56d08c680afc2d6a28
Mahad Hameed <mahad095@gmail.com>
2023-10-08 00:00:47 +0500

    first commit

---
+++ lib/Data.ts
@@ -0,0 +1,16 @@
+const textArray = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac fringilla elit. Proin dapibus lorem nec justo tristique, sed aliquet justo ullamcorper.",
+    
+    "Suspendisse potenti. Vivamus eget erat eu odio sagittis accumsan. Vestibulum eget feugiat ligula, ac tincidunt libero. Integer dignissim felis vel libero facilisis.",
+    
+    "Fusce eget dui quis justo ullamcorper feugiat. Nullam sit amet dolor a massa facilisis condimentum. Phasellus varius, mi at interdum congue, nunc velit rhoncus velit,",
+    
+    "Curabitur ac velit ullamcorper, posuere libero nec, interdum mauris. Sed euismod, erat et malesuada fermentum, ligula odio fringilla nisl, ac convallis est dolor at justo.",
+    
+    "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut bibendum augue ut suscipit luctus. Nam hendrerit libero in nisi laoreet,",
+    
+    "Aenean placerat velit id purus condimentum, vel fringilla ante mattis. Suspendisse non est quis justo luctus auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere.",
+    
+    "Nunc congue lorem eget lorem mattis, sit amet malesuada velit feugiat. Donec non tincidunt ligula. Vestibulum ante ipsum primis in faucibus"
+]
+  
