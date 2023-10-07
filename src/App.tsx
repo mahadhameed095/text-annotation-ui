b0778aead57b3b211d0a1d2bb60c397ece1675ea
RaahimSiddiqi <k200146@nu.edu.pk>
2023-10-08 01:56:05 +0500

    Added Navigation and Home Page (empty)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import AnnotationTool  from "../pages/AnnotationTool";
+import Home from "../pages/Home";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function App() {
-
   return (
-    <AnnotationTool></AnnotationTool>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>  
+          <Route path="/tool" element={<AnnotationTool/>}></Route>  
+          <Route path="*" element={<Home/>}></Route>  
+        </Routes>
+    </Router>
   )
 }
