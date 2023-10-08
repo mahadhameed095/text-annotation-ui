06b7326d075617aef74fee626660ed19157c4695
RaahimSiddiqi <k200146@nu.edu.pk>
2023-10-08 13:45:34 +0500

    Style changes for mobile, and ease of use

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
-
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( 
-        <div className="px-16 container mx-auto mt-5"> 
-            <h1 className="text-3xl font-bold underline">Your Website</h1>
-            <p>Meow</p>
-        </div>
+        <>
+        <header className="bg-blue-500 py-4 flex">
+            <div className="container my-auto mx-auto">
+                <h1 className="text-white text-2xl px-8 font-semibold">AnnoText</h1>
+            </div>
+            <div className="pr-4 md:pr-12">
+                <Link to="/tool">
+                    <button className="btn bg-white rounded shadow w-44 h-12 hover:bg-gray-200">To Annotation Tool</button>
+                </Link>
+            </div>
+        </header>
+    
+        </>
      );
 }
  
