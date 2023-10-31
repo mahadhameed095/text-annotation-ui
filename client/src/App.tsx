f11b8cd58a9c18740619d67e120604cd35b6d515
Mahad Hameed <mahad095@gmail.com>
2023-10-31 12:34:14 +0500

    trying to fix base url issues in deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { Toaster } from "@/components/ui/toaster";
 import Header from "./components/Header";
 import { userContext, userType } from "./context";
 import { useState } from "react";
-
 export default function App() {
   const [user, setUser] = useState<userType | null>(null);
 
@@ -21,7 +20,7 @@ export default function App() {
 
   return (
     <userContext.Provider value={{user, login, logout}}>
-      <Router>
+      <Router basename='/text-annotation-ui/'>
           <Header />
           <Routes>
             <Route path="/" element={<Home/>} /> 
