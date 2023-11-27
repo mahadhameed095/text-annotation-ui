2ea5851add9dd948332a6b93dcf4e113ab5d18ce
Mahad Hameed <mahad095@gmail.com>
2023-11-27 12:59:51 +0500

    bundled, and minified the server for fast deployments.

---
+++ server/config.ts
@@ -0,0 +1,5 @@
+import path from "path";
+
+export default {
+    rootPath : path.resolve(__dirname)
+}
--- server/startup.ts
+++ server/startup.ts
@@ -1,18 +1,17 @@
 import prismaClient from "./prisma";
-import { createAdmin, getUserById } from "./service/User.service";
 
-const uids = [
-    "6S6dh8JRJraLdNC6pxg8wr8Desm2", //mahad
-    "SM78hkQxsqT3JYWW16faV1IV4642", //raahim
-    "VlqWoer8jvZ9XRcYEMfEfrblup03", //ahmed
-]
-
-const admin = { approved : true, role : "ADMIN" } as const;
-
-prismaClient.$transaction(
-    uids.map(id => prismaClient.user.upsert({
-        where : { id },
-        create : { id, ...admin},
-        update : admin,
-    }))
-).then(() => console.log("---------------start up script executed------------------"));
+export default async function startup(){
+    const uids = [
+        "6S6dh8JRJraLdNC6pxg8wr8Desm2", //mahad
+        "SM78hkQxsqT3JYWW16faV1IV4642", //raahim
+        "VlqWoer8jvZ9XRcYEMfEfrblup03", //ahmed
+    ];
+    const admin = { approved : true, role : "ADMIN" } as const;
+    await prismaClient.$transaction(
+        uids.map(id => prismaClient.user.upsert({
+            where : { id },
+            create : { id, ...admin},
+            update : admin,
+        }))
+    );
+}
