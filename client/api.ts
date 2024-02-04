631eb8b8b9d6d2aceab39b310808b49e81c7bea2
RaahimSiddiqi <k200146@nu.edu.pk>
2024-02-04 22:16:30 +0500

    Auth Loading UI fix

--- client/api.ts
+++ client/api.ts
@@ -5,7 +5,7 @@ export * from '../server/schemas';
 
 export type Labels = Value;
 
-const baseUrl =  "https://annotext.azurewebsites.net";
+const baseUrl =  "http://127.0.0.1:5433";
 
 export const User = initClient(ApiContract.user, {
     baseUrl,
